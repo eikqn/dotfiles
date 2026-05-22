7621d862d75882170c207fb82cb194d6ea619529
Vitor Eiken <117394582+eikqn@users.noreply.github.com>
2026-05-22 00:52:38 -0300

    Add init.lua
    Starting to set up Neovim from scratch, adding only what I need

---
+++ nvim/init.lua
@@ -0,0 +1,56 @@
+-- PLUGINS
+vim.pack.add{
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+}
+
+require("mason").setup()
+require("nvim-tree").setup()
+
+-- OPTIONS
+vim.g.mapleader = ' ' -- set <space> as the leader key
+
+vim.o.termguicolors = true
+vim.cmd.colorscheme("catppuccin-mocha")
+
+vim.o.number = true -- show line numbers in a column.
+vim.o.relativenumber = false -- show line numbers relative to where the cursor is.
+
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+
+vim.o.cursorline = true -- Highlight the line where the cursor is on.
+vim.o.scrolloff = 10 -- Keep this many screen lines above/below the cursor.
+
+vim.o.winborder = "rounded"
+
+-- KEYMAPS
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+vim.keymap.set("n", "<leader>ef", ":NvimTreeToggle<CR>")
+
+-- LSPs
+vim.lsp.config('lua_ls', {})
+vim.lsp.enable({'lua_ls', 'clangd', 'jdtls'})
